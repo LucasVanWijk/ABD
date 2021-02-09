e7168d4f911e75f45d1354674da80018f2d39cea
ThomasHoek <hoek.thomas@gmail.com>
2021-02-09 15:19:11 +0100

    werkende simulatie

--- Group/agents.py
+++ Group/agents.py
@@ -1,27 +1,69 @@
-def agent():
-    #TODO has a agent which moves to a random square every tick
-    infected = None
-    
-    def check_neighbours(self):
-        neighbours = self.model.grid.get_cell_list_contents([self.pos])
-        for person in neighbours:
-            if person.infected:
-                return True
-        return False
-        
+from mesa import Agent, Model
+from mesa.time import SimultaneousActivation
+from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
+
+def compute_infected(model):
+    return sum([1 if agent.infected else 0 for agent in model.schedule.agents ])
+
+class MoneyAgent(Agent):
+    """ An agent with fixed initial wealth."""
+    def __init__(self, unique_id, model,infected=False):
+        super().__init__(unique_id, model)
+        self.infected = infected
+
     def move(self):
-        #  https://mesa.readthedocs.io/en/master/tutorials/intro_tutorial.html
-
         possible_steps = self.model.grid.get_neighborhood(
-        self.pos,
-        moore=True,
-        include_center=False)
-
+            self.pos,
+            moore=True,
+            include_center=False)
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
 
+    def infect_other(self):
+        surround = self.model.grid.get_neighborhood(self.pos,moore=True,include_center=True)
+        for cell in surround:
+            cellmates = self.model.grid.get_cell_list_contents([cell])
+            if len(cellmates) > 0:
+                for agent in cellmates:
+                    agent.infected = True
+
     def step(self):
         self.move()
-        if self.check_neighbours(): #TODO if neigbor is infected become infected
-            self.infected = True
+        if self.infected:
+            self.infect_other()
+
+
+class MoneyModel(Model):
+    """A model with some number of agents."""
+    def __init__(self, healthy_N, sick_N, width, height):
+        self.healthy_agents = healthy_N
+        self.sick_agent = sick_N
+        self.grid = MultiGrid(width, height, True)
+        self.schedule = SimultaneousActivation(self)
+        self.running = True
         
+        # infected agent
+        for i in range(self.sick_agent):
+            a = MoneyAgent(i, self,True)
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+
+        # Create healthy agents
+        for i in range(self.sick_agent,self.sick_agent+ self.healthy_agents):
+            a = MoneyAgent(i, self)
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+        
+        self.datacollector = DataCollector(
+            model_reporters={"infected": compute_infected})
+            
+    def step(self):
+        self.datacollector.collect(self)
+        self.schedule.step()
--- Group/visualisation.py
+++ Group/visualisation.py
@@ -1,5 +1,44 @@
-def visu():
-    #world = Get world
-    #Make agents
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import ChartModule
+from agents import *
 
-    #visulize
+def agent_portrayal(agent):
+    portrayal = {"Shape": "circle",
+                 "Filled": "true",
+                 "r": 0.5}
+
+    if agent.infected:
+        portrayal["Color"] = "green"
+        portrayal["Layer"] = 0
+    else:
+        portrayal["Color"] = "red"
+        portrayal["Layer"] = 1
+        
+    return portrayal
+
+# grid = CanvasGrid(agent_portrayal, vakjes_x, vakjes_y, aantal_x_pixel, aantal_y_pixel)
+grid = CanvasGrid(agent_portrayal, 100, 100, 1000, 1000)
+
+chart = ChartModule([{"Label": "infected",
+                      "Color": "Green"}],
+                    data_collector_name='datacollector')
+
+
+server = ModularServer(MoneyModel,
+                       [grid,chart],
+                       "Money Model",
+                    #    {"N":250,"width":100, "height":100})
+                       {"healthy_N":250,"sick_N":10, "width":100, "height":100})
+server.port = 8521 # The default
+server.launch()
+
+
+
+# from agents import MoneyModel
+# import matplotlib.pyplot as plt
+# import numpy as np
+ 
+# model = MoneyModel(healthy_N=3,sick_N=2, width=2, height=2)
+# for i in range(1):
+#     model.step()
