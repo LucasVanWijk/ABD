3c5a40b539407574a39d3acb6f5f0bf1bfa0935e
ThomasHoek <hoek.thomas@gmail.com>
2021-02-09 10:42:29 +0100

    agent v1

--- Group/agents.py
+++ Group/agents.py
@@ -2,7 +2,26 @@ def agent():
     #TODO has a agent which moves to a random square every tick
     infected = None
     
-    def becomeInfected():
-    #TODO if neigbor is infected become infected
+    def check_neighbours(self):
+        neighbours = self.model.grid.get_cell_list_contents([self.pos])
+        for person in neighbours:
+            if person.infected:
+                return True
+        return False
+        
+    def move(self):
+        #  https://mesa.readthedocs.io/en/master/tutorials/intro_tutorial.html
 
-    
+        possible_steps = self.model.grid.get_neighborhood(
+        self.pos,
+        moore=True,
+        include_center=False)
+
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+    def step(self):
+        self.move()
+        if self.check_neighbours(): #TODO if neigbor is infected become infected
+            self.infected = True
+        
