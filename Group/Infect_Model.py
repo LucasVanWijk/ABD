d43c9c8d3f8296d0efc7b892d718a7fe77ca7ad0
Carlo Keizer <ck.soest@hotmail.com>
2021-02-10 13:17:50 +0100

    Save cells with city, village, recreations

--- Group/Infect_Model.py
+++ Group/Infect_Model.py
@@ -16,15 +16,18 @@ def build_city_village_rec(model, start_h, start_w, size, type_build):
             if type_build == "city":
                 city = City(int(str(h) + "0" + str(w)), model)
                 model.grid.place_agent(city, (w, h))
+                model.cities.append((w, h))
             elif type_build == "village":
                 village = City(int(str(h) + "0" + str(w)), model)
                 model.grid.place_agent(village, (w, h))
+                model.villages.append((w, h))
             elif type_build == "recreation":
                 recreation = City(int(str(h) + "0" + str(w)), model)
                 model.grid.place_agent(recreation, (w, h))
+                model.recreations.append((w, h))
 
 
-class Base_Model(Model):
+class BaseModel(Model):
     """A model with some number of agents."""
     def __init__(self, healthy_N, sick_N, width, height, min_per_step=1,  ini_date=datetime.datetime(2020, 1, 1, 00, 00)):
         self.healthy_agents = healthy_N
@@ -35,6 +38,9 @@ class Base_Model(Model):
         self.schedule = SimultaneousActivation(self)
         self.running = True
         self.date = ini_date
+        self.cities = []
+        self.villages = []
+        self.recreations = []
 
         # build cities
         build_city_village_rec(self, 5, 4, 7, "city")
