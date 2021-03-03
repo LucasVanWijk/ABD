c691ded221920c33bbbbbabb675b58c9f0a337b5
lucas van Wijk <47404162+LucasVanWijk@users.noreply.github.com>
2021-03-03 11:12:38 +0100

    Added task

--- Group/demographic.py
+++ Group/demographic.py
@@ -2,8 +2,9 @@ class demo():
     behaviorDict = {}
 
     def getAction(self, time):
+        # Gegeven een tijd geef terug wat deze agent wil doen en hoe belangrijk hij dit vind
         return self.behaviorDict.get(time)
 
 class student(demo):
-    behaviorDict = {"9:00": (True, "School"),
+    behaviorDict = {"9:00": (100, "School"),
                     "15:00": (False, "Recreation")}
