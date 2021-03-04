03f05bd46c59035b505ef4a38e495e938e2265bd
lucas van Wijk <47404162+LucasVanWijk@users.noreply.github.com>
2021-03-04 16:01:47 +0100

    rewrote to take advantage of new network_type atribute

--- Group/Infect_Agents.py
+++ Group/Infect_Agents.py
@@ -16,35 +16,17 @@ class Infect_Agent(Agent):
         self.demo = demo_class
         self.current_loc = None
 
-        def get_sub_network(full_network, node_type, source):
-            #Given the full network x network return a sub network where all nodes are of the given type
-            #And where the source (node) is present
-            return full_network
-
-        
-        def find_nearest(G, typeofnode, fromnode):
-            subset_typeofnode = lambda G, typestr : [name for name, d in G.nodes(data=True) if 'type' in d and (d['type'] ==typestr)]
-            #Calculate the length of paths from fromnode to all other nodes
-            lengths=nx.single_source_dijkstra_path_length(G, fromnode, weight='distance')
-            paths = nx.single_source_dijkstra_path(G, fromnode)
-
-            #We are only interested in a particular type of node
-            subnodes = subset_typeofnode(G, typeofnode)
-            subdict = {k: v for k, v in lengths.items() if k in subnodes}
-
-            #return the smallest of all lengths to get to typeofnode
-            if subdict: #dict of shortest paths to all entrances/toilets
-                nearest =  min(subdict, key=subdict.get) #shortest value among all the keys
-                return nearest
-            else: #not found, no path from source to typeofnode
-                return(None, None, None)
 
 
-        def find(node_source, type_of_node, all_nodes):
-            all_paths =  nx.shortest_path(all_nodes, node_source)    
-            #shortest_path = min(all_paths, key=all_paths.get)
-            v = list(all_paths.values())
-            return all_paths[v[0][0]]    
+        def find(node_source, type_of_node, model):
+            all_nodes = model.temp_node
+            network = model.G
+            shortest = [0] * 99
+            for t in all_nodes:
+                shortest_path =  nx.shortest_path(network, source=node_source, target=t)
+                if len(shortest_path) < len(shortest):
+                    shortest = shortest_path
+                return shortest[-1]
 
 
         def pop_closest_dict(home):
@@ -52,8 +34,7 @@ class Infect_Agent(Agent):
             network_types.remove("House")
             closest_dict = {"House": home}
             for n_type in network_types:
-                sub_network = get_sub_network(self.model.G, n_type, home)
-                closest_dict[n_type] = find(home, n_type, sub_network)
+                closest_dict[n_type] = find(home, n_type, model)
             
             return closest_dict
 
